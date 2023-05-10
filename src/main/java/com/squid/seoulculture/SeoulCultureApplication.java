210c9fb568cf1cc102879d2548fb8624da89db70
wisdom5565 <wisdom5565@naver.com>
2023-05-10 23:40:09 +0900

    [기본설정] SpringBoot 기본 설정 프로젝트

---
+++ src/main/java/com/squid/seoulculture/SeoulCultureApplication.java
@@ -0,0 +1,13 @@
+package com.squid.seoulculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulCultureApplication.class, args);
+	}
+
+}
