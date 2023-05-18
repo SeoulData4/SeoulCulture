7c90e31b79448988dce55654302655ebd5bf8392
juy <61074722+juyeon1570@users.noreply.github.com>
2023-05-18 22:18:52 +0900

    오타 수정

--- src/main/java/com/squad/seoulculture/controller/CultureController.java
+++ src/main/java/com/squad/seoulculture/controller/CultureController.java
@@ -1,16 +1,15 @@
-package com.squid.seoulculture.controller;
+package com.squad.seoulculture.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/")
-public class CulturalController {
+public class CultureController {
 
     @GetMapping("")
     public String index () {
