1033ef9c5e77ba10efe817be6611216c03c495d6
ghdwhd0302 <ghdwhd0302@naver.com>
2023-06-01 22:03:10 +0900

    culture list

---
+++ src/main/java/com/squad/seoulculture/repository/SebcHistoricSiteRowRepository.java
@@ -0,0 +1,9 @@
+package com.squad.seoulculture.repository;
+
+import com.squad.seoulculture.domain.SebcHistoricSiteRow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SebcHistoricSiteRowRepository extends JpaRepository<SebcHistoricSiteRow, String>  {
+
+}
