285a4280c697f63194b9d54c9dda2b8be16cdb46
juy <61074722+juyeon1570@users.noreply.github.com>
2023-06-19 00:53:38 +0900

    리뷰파트 레파지토리 생성 및 페이지 맵핑

---
+++ src/main/java/com/squad/seoulculture/repository/ReviewNoneRepository.java
@@ -0,0 +1,7 @@
+package com.squad.seoulculture.repository;
+
+import com.squad.seoulculture.domain.ReviewNone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewNoneRepository extends JpaRepository<ReviewNone, Long> {
+}
